3e13182e1c0543917f8d9a3ba25d046813001802
niclasrst <nxclxs.rist@gmail.com>
2018-03-19 22:26:53 +0100

    renew

--- js/scroll.js
+++ js/scroll.js
@@ -1,61 +1,25 @@
 // JavaScript Document
 
 // ######################################################################################
-// Responsive
-// ######################################################################################
-
-$(document).ready(function() {
-
-	ud_resp_leistung();
-	ud_resp_about();
-
-	$(window).resize(function() {
-       	ud_resp_leistung();
-		ud_resp_about();
-      });
-
-	function ud_resp_about() {
-
-		var UD_HEIGHT = $(window).height();
-		var UD_WIDTH = $(window).width();
-
-		if(UD_WIDTH > 800) {
-			$('.ud_UberUns').css('height',(UD_HEIGHT-((UD_HEIGHT/100)*5)-240));
-		} else {
-			$('.ud_UberUns').css('min-height',150);
-		}
-
-	}
-
-	function ud_resp_leistung() {
-
-		var UD_HEIGHT = $(window).height();
-		var UD_WIDTH = $(window).width();
-
-		if(UD_WIDTH > 1150) {
-			$('.ud_leistungen ul li').css('height',(UD_HEIGHT-((UD_HEIGHT/100)*5)-240));
-		} else {
-			$('.ud_leistungen ul li').css('min-height',350);
-		}
-
-	}
-
-  });
-
-// ######################################################################################
 // Scrolling
 // ######################################################################################
 
 $(function(){
-	$('a[href*=#]').stop().click(function(){
-		if(location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
-			var UD_HASH = this.hash;
-			var UD_ZIEL = $(this.hash);
-			if(UD_ZIEL.length){
-				var UD_ABSTAND_TOP = UD_ZIEL.offset().top;
-				$('html,body').animate({scrollTop: UD_ABSTAND_TOP}, 750,function(){
-					window.location.hash = UD_HASH;
+	$('a[href*=#]').stop().click(function () {
+
+		if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+
+			var HASH = this.hash;
+			var ZIEL = $(this.hash);
+
+			if (ZIEL.length) {
+
+				var ABSTAND_TOP = ZIEL.offset().top + 60;
+
+				$('html,body').animate({scrollTop: ABSTAND_TOP}, 750, function () {
+					window.location.hash = HASH;
 				});
+
 				return false;
 			}
 		}
